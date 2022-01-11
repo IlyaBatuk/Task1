e436b864a551d6baf54fce1aafdf37e37610647e
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-11 12:34:31 +0300

    Corrected filter for find first 5 tasks with Type reading

--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -6,6 +6,7 @@ import entity.TaskType;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,24 +15,25 @@ public class TaskService {
     public List<Task> initListTasks() {
 
         Task task1 = new Task.Builder("1", "Read Version Control with Git book", TaskType.READING,
-                LocalDate.of(2015, Month.JULY, 1)).addTag("git").addTag("reading").addTag("books").build();
+                LocalDate.of(2015, Month.JULY, 4)).addTag("git").addTag("reading").addTag("books").build();
 
         Task task2 = new Task.Builder("2", "Read Java 8 Lambdas book", TaskType.READING,
-                LocalDate.of(2015, Month.JULY, 2)).addTag("java8").addTag("reading").addTag("books").build();
+                LocalDate.of(2015, Month.JULY, 5)).addTag("java8").addTag("reading").addTag("books").build();
 
         Task task3 = new Task.Builder("3", "Write a mobile application to store my tasks", TaskType.CODING,
-                LocalDate.of(2015, Month.JULY, 3)).addTag("coding").addTag("mobile").build();
+                LocalDate.of(2015, Month.JULY, 2)).addTag("coding").addTag("mobile").build();
 
         Task task4 = new Task.Builder("4", "Write a blog on Java 8 Streams", TaskType.WRITING,
-                LocalDate.of(2015, Month.JULY, 4)).addTag("blogging").addTag("writing").addTag("streams").build();
+                LocalDate.of(2015, Month.JULY, 1)).addTag("blogging").addTag("writing").addTag("streams").build();
 
         Task task5 = new Task.Builder("5", "Read Domain Driven Design book", TaskType.READING,
-                LocalDate.of(2015, Month.JULY, 5)).addTag("ddd").addTag("books").addTag("reading").build();
+                LocalDate.of(2015, Month.JULY, 3)).addTag("ddd").addTag("books").addTag("reading").build();
 
         return Arrays.asList(task1, task2, task3, task4, task5);
     }
 
-    public List<Task> findFirst5TasksWithTypeReading(List<Task> tasks){
-        return tasks.stream().filter(t -> t.getType().equals(TaskType.READING)).limit(5).toList();
+    public List<String> findFirst5TasksWithTypeReading(List<Task> tasks){
+        return tasks.stream().filter(t -> t.getType().equals(TaskType.READING)).limit(5)
+                .sorted(Comparator.comparing(Task::getCreatedOn)).map(Task::getTitle).toList();
     }
 }
