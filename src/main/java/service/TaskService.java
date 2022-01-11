77d23951cb19ef3ac5cefb6c9ec44656c34c1e8d
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-11 13:21:38 +0300

    Created group by any 4 parameters

--- src/main/java/service/TaskService.java
+++ src/main/java/service/TaskService.java
@@ -8,6 +8,7 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class TaskService {
@@ -32,8 +33,24 @@ public class TaskService {
         return Arrays.asList(task1, task2, task3, task4, task5);
     }
 
-    public List<String> findFirst5TasksWithTypeReading(List<Task> tasks){
+    public List<String> findFirst5TasksWithTypeReading(List<Task> tasks) {
         return tasks.stream().filter(t -> t.getType().equals(TaskType.READING)).limit(5)
                 .sorted(Comparator.comparing(Task::getCreatedOn)).map(Task::getTitle).toList();
+    }
+
+    public Map<String, List<Task>> groupTasksById(List<Task> tasks) {
+        return tasks.stream().collect(Collectors.groupingBy(Task::getId));
+    }
+
+    public Map<TaskType, List<Task>> groupTasksByType(List<Task> tasks) {
+        return tasks.stream().collect(Collectors.groupingBy(Task::getType));
+    }
+
+    public Map<LocalDate, List<Task>> groupTasksByCreation(List<Task> tasks) {
+        return tasks.stream().collect(Collectors.groupingBy(Task::getCreatedOn));
+    }
+
+    public Map<Boolean, List<Task>> groupTasksByIsDone(List<Task> tasks) {
+        return tasks.stream().collect(Collectors.groupingBy(Task::isDone));
     }
 }
