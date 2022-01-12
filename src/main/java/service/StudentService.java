c1c6efc8bfb31ca1e52a1163b4c2e295faa3d2e8
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-12 17:10:34 +0300

    commit

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -3,70 +3,15 @@ package service;
 import entity.Student;
 
 import java.util.Comparator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
+
+import static java.util.Comparator.comparing;
 
 public class StudentService {
 
-    public List<Student> initListStudents() {
-        List<Student> students = new LinkedList<>();
-
-        Student student1 = new Student("Bob");
-        Student student2 = new Student("Maria");
-        Student student3 = new Student("Tom");
-        Student student4 = new Student("Ilya");
-        Student student5 = new Student("Nikolay");
-
-        student1.rate("Math", 7);
-        student1.rate("Physics", 8);
-        student2.rate("Math", 8);
-        student2.rate("Physics", 7);
-        student3.rate("Math", 6);
-        student3.rate("Physics", 6);
-        student4.rate("Math", 9);
-        student4.rate("Physics", 8);
-        student5.rate("Math", 5);
-        student5.rate("Physics", 5);
-
-        students.add(student1);
-        students.add(student2);
-        students.add(student3);
-        students.add(student4);
-        students.add(student5);
-
-        return students;
-    }
-
-    public Double averageRatingForSubject(List<Student> students, String subjectName) {
-        return students.stream().map(Student::getRating).mapToDouble(d -> d.get(subjectName))
-                .average().orElseThrow();
-    }
-
-    public void printStudentsAndSubjects(List<Student> students) {
-        List<String> subjects = new LinkedList<>();
-
-        students.stream().map(Student::getRating)
-                .map(Map::keySet).forEach(subjects::addAll);
-        subjects = subjects.stream().distinct().collect(Collectors.toList());
-
-        for (String subject : subjects) {
-            List<String> forPrintStudentsAndSubjects = new LinkedList<>();
-            System.out.print(subject + ": ");
-
-            students.stream().filter(subj -> subj.getRating().get(subject) != null)
-                    .forEach(student -> forPrintStudentsAndSubjects.add(student.getName()
-                            + ": " + student.getRating().get(subject)));
-
-            System.out.println(forPrintStudentsAndSubjects);
-
-        }
-    }
-
     public void findBestMatchingPerson(List<Student> students, String subject, Integer rating) {
-        if (students.stream().anyMatch(student -> student.getRating().containsKey(subject))) {
-            if (students.stream().anyMatch(student -> student.getRating().get(subject) >= rating)) {
+        if (isExistSubject(students, subject)) {
+            if (isExistStudentsWithRating(students, subject, rating)) {
                 System.out.print(subject + " : ");
                 students.stream()
                         .filter(student -> student.getRating().get(subject) >= rating)
@@ -74,12 +19,22 @@ public class StudentService {
                         .forEach(name -> System.out.print(name + "; "));
             } else {
                 students.stream()
-                        .max(Comparator.comparing(student -> student.getRating().get(subject)))
+                        .max(comparing(student -> student.getRating().get(subject)))
                         .ifPresent(student -> System.out.println(subject + " : " + student.getName()));
             }
         } else {
             System.out.println("Subject not found");
         }
 
+    }
+
+    private boolean isExistSubject(List<Student> students, String subject){
+        return students.stream()
+                .anyMatch(student -> student.getRating().containsKey(subject));
+    }
+
+    private boolean isExistStudentsWithRating(List<Student> students, String subject, Integer rating){
+        return students.stream()
+                .anyMatch(student -> student.getRating().get(subject) >= rating);
     }
 }
