99442961072b0cbb74a797fc7812a1ba998bab2f
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-12 14:34:23 +0300

    commit

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -2,10 +2,40 @@ package service;
 
 import entity.Student;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
 public class StudentService {
+
+    public List<Student> initListStudents() {
+        List<Student> students = new LinkedList<>();
+
+        Student student1 = new Student("Bob");
+        Student student2 = new Student("Maria");
+        Student student3 = new Student("Tom");
+        Student student4 = new Student("Ilya");
+        Student student5 = new Student("Nikolay");
+
+        student1.rate("Math", 7);
+        student1.rate("Physics", 8);
+        student2.rate("Math", 8);
+        student2.rate("Physics", 7);
+        student3.rate("Math", 6);
+        student3.rate("Physics", 6);
+        student4.rate("Math", 9);
+        student4.rate("Physics", 8);
+        student5.rate("Math", 5);
+        student5.rate("Physics", 5);
+
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+        students.add(student4);
+        students.add(student5);
+
+        return students;
+    }
 
     public Double averageRatingForSubject(List<Student> students, String subjectName) {
         return students.stream()
