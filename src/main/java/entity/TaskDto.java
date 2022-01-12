825f184f057c6f8492c2a262240ecc6dfb9bc46c
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-12 11:50:59 +0300

    Commit changes

--- src/main/java/entity/TaskDto.java
+++ src/main/java/entity/TaskDto.java
@@ -9,11 +9,10 @@ import java.util.Set;
 @Data
 @AllArgsConstructor
 public class TaskDto {
+
     private final String id;
     private final String title;
     private final TaskType type;
     private final LocalDate createdOn;
     private Set<String> tags;
-
-
 }
