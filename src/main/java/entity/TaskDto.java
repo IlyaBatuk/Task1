bdd4eeb8d0570bb6db50836d9a61586efdd5ce86
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-12 10:02:50 +0300

    Deleted lines

--- src/main/java/entity/TaskDto.java
+++ src/main/java/entity/TaskDto.java
@@ -2,18 +2,16 @@ package entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
 import java.time.LocalDate;
 import java.util.Set;
 
 @Data
 @AllArgsConstructor
 public class TaskDto {
+
     private final String id;
     private final String title;
     private final TaskType type;
     private final LocalDate createdOn;
     private Set<String> tags;
-
-
 }
