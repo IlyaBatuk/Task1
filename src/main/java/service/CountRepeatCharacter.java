871cece5b01d653b43f60737652babb8cafb4d0b
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-12 13:16:12 +0300

    New method countMaxRepeatChar

--- src/main/java/service/CountRepeatCharacter.java
+++ src/main/java/service/CountRepeatCharacter.java
@@ -1,34 +1,20 @@
 package service;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class CountRepeatCharacter {
-
-    public Map<Character, Integer> countMaxRepeatChar(String str) {
-        StringBuilder sb = new StringBuilder();
-        Map<Integer, Character> countAllChar = new HashMap<>();
-
-        for (int i = 0; i < str.length(); i++) {
-            if (Character.isLetterOrDigit(str.charAt(i))) {
-                sb.append(str.charAt(i));
-            }
-        }
-
-        char[] chars = sb.toString().toCharArray();
-        int count = 0;
-
-        for (char c : chars) {
-            for (char rep : chars) {
-                if (rep == c)
-                    count++;
-            }
-            countAllChar.put(count, c);
-            count = 0;
-        }
-
-        int value = Collections.max(countAllChar.keySet());
-        char key = countAllChar.get(value);
-
-        return Collections.singletonMap(key, value);
+    public void countMaxRepeatChar(String str) {
+        str.chars()
+                .filter(Character::isLetterOrDigit)
+                .mapToObj(character -> (char) character)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .ifPresentOrElse(entry -> System.out.println("[\"" + entry.getKey() + "\", "
+                                + entry.getValue() + "]"),
+                        () -> System.out.println("String is empty"));
     }
 }
