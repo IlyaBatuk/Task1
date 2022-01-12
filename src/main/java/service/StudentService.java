097c2f6b6369369a51065b53d021d9eb333839c2
IlyaBatuk <ilya.batuk@leverx.com>
2022-01-12 11:27:38 +0300

    Changed method printStudentsAndSubjects

--- src/main/java/service/StudentService.java
+++ src/main/java/service/StudentService.java
@@ -2,35 +2,30 @@ package service;
 
 import entity.Student;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.List;
+import java.util.Objects;
 
 public class StudentService {
 
 
-    public Double averageRatingForSubject(List<Student> students, String subjectName){
-        return students.stream().map(Student::getRating).mapToDouble(d -> d.get(subjectName))
-                .average().orElseThrow();
+    public Double averageRatingForSubject(List<Student> students, String subjectName) {
+        return students.stream()
+                .map(Student::getRating)
+                .mapToDouble(d -> d.get(subjectName))
+                .average()
+                .orElseThrow(() -> new RuntimeException("Subject not found"));
     }
 
-    public void printStudentsAndSubjects(List<Student> students){
-        List<String> subjects = new LinkedList<>();
+    public void printStudentsAndSubjects(List<Student> students) {
 
-
-        students.stream().map(Student::getRating)
-                .map(Map::keySet).forEach(subjects::addAll);
-        subjects = subjects.stream().distinct().collect(Collectors.toList());
-
-        for(String subject : subjects){
-            List<String> forPrintStudentsAndSubjects = new LinkedList<>();
-            System.out.print(subject + ": ");
-
-            students.stream().filter(subj -> subj.getRating().get(subject) != null)
-                    .forEach(student -> forPrintStudentsAndSubjects.add(student.getName()
-                            + ": " + student.getRating().get(subject)));
-
-            System.out.println(forPrintStudentsAndSubjects);
-
-        }
+        students.stream()
+                .map(Student::getRating)
+                .flatMap(stringIntegerMap -> stringIntegerMap.keySet().stream())
+                .distinct()
+                .peek(subject -> System.out.print("\n" + subject + " = "))
+                .forEach(subject -> students.stream()
+                        .filter(subj -> Objects.nonNull(subj.getRating().get(subject)))
+                        .forEach(student -> System.out.print(student.getName() + ": "
+                                + student.getRating().get(subject) + "; ")));
     }
 }
